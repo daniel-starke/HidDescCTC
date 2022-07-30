d00772b32314b8a9b101965d45cd40cb3b156e4d
Daniel Starke <daniel-email@gmx.net>
2022-07-30 21:52:03 +0200

    Fixed missing wasm-ld in Github publish action.
    Signed-off-by: Daniel Starke <daniel-email@gmx.net>

--- etc/build.sh
+++ etc/build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+CXX=clang++
+
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz || exit 1
+wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/libclang_rt.builtins-wasm32-wasi-16.0.tar.gz || exit 1
+
 tar -xvzf wasi-sysroot-16.0.tar.gz || exit 1
-clang++ --version
-clang++ --sysroot=wasi-sysroot --target=wasm32-wasi -Dconstexpr= -O3 -nostdlib++ -nostartfiles -fno-exceptions -Wl,--no-entry -Wl,--export-dynamic -Wl,--import-memory -fvisibility=hidden -s -o ../docs/assets/HidWebCompiler.wasm HidWebCompiler.cpp || exit 1
+sudo tar -xvzf libclang_rt.builtins-wasm32-wasi-16.0.tar.gz -C $(dirname $(dirname $(dirname $(${CXX} -rtlib=compiler-rt --target=wasm32-wasi --print-libgcc-file-name)))) || exit 1
+
+${CXX} --version
+${CXX} --sysroot=wasi-sysroot --target=wasm32-wasi -Dconstexpr= -O3 -nostdlib++ -nostartfiles -fno-exceptions -Wl,--no-entry -Wl,--export-dynamic -Wl,--import-memory -fvisibility=hidden -s -o ../docs/assets/HidWebCompiler.wasm HidWebCompiler.cpp || exit 1
