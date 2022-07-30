f714e315ff8e16ba501ea8d16a89fbac96f64cb6
Daniel Starke <daniel-email@gmx.net>
2022-07-30 20:39:29 +0200

    Fixed WASM build and godbolt link.
    Signed-off-by: Daniel Starke <daniel-email@gmx.net>

--- etc/build.sh
+++ etc/build.sh
@@ -2,4 +2,5 @@
 
 wget https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sysroot-16.0.tar.gz || exit 1
 tar -xvzf wasi-sysroot-16.0.tar.gz || exit 1
-clang++ --sysroot=wasi-sysroot --target=wasm32-wasi -O3 -nostdlib++ -nostartfiles -fno-exceptions -Wl,--no-entry -Wl,--export-dynamic -Wl,--import-memory -fvisibility=hidden -s -o ../docs/assets/HidWebCompiler.wasm HidWebCompiler.cpp || exit 1
+clang++ --version
+clang++ --sysroot=wasi-sysroot --target=wasm32-wasi -Dconstexpr= -O3 -nostdlib++ -nostartfiles -fno-exceptions -Wl,--no-entry -Wl,--export-dynamic -Wl,--import-memory -fvisibility=hidden -s -o ../docs/assets/HidWebCompiler.wasm HidWebCompiler.cpp || exit 1
